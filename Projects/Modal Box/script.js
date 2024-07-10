c4d6f4f4fe21fbbba1c7fd86a27bcbd29141a856
Assssaddd <78721283+Assssaddd@users.noreply.github.com>
2024-07-10 21:05:39 +0530

    Add couple of JavaScript projects
    Add two Javascript projects.
    
    1. "The Daily Directive"
    2. "Modal Box"

---
+++ Projects/Modal Box/script.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('button')
+const close = document.querySelector('.close-icon')
+const popup = document.querySelector('.popup-container')
+const overlay = document.querySelector('.overlay')
+
+
+button.addEventListener('click', () => {
+    popup.classList.add('popup-open')
+})
+
+close.addEventListener('click', () => {
+    popup.classList.remove('popup-open')
+})
+
+overlay.addEventListener('click', () => {
+    popup.classList.remove('popup-open')
+})
